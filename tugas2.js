7e540631fab343df734e8227d61a7822a23e985c
Iwiwpu <literellijus@gmail.com>
2020-08-22 22:59:47 +0700

    Add completed JS

---
+++ tugas2.js
@@ -0,0 +1,13 @@
+var number = 12345;
+var string = "Hello World";
+var boolean = true;
+var empty = null;
+var notExist = undefined;
+var array = [10, 20, 30, 40, 50];
+
+console.log(number);
+console.log(string);
+console.log(boolean);
+console.log(empty);
+console.log(notExist);
+console.log(array);
